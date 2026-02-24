e3643d7cd87378ffd5fc02ad5da8550aca5f339a
vicTOOOOORRRRR <victort3828@gmail.com>
2026-02-24 14:16:16 -0800

    local server online

---
+++ src/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Weather Task API is running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
