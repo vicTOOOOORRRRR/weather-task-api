08d9b5350aa842bbc766856978ff6e831df437a1
vicTOOOOORRRRR <victort3828@gmail.com>
2026-03-03 14:31:13 -0800

    updated frontend view, shows suggestions of places when user types city name and other things

--- src/app.js
+++ src/app.js
@@ -1,61 +1,77 @@
-require("dotenv").config();
-const express = require("express");
+require("dotenv").config(); //loads the .env
 
+//import and setup express
+const express = require("express");
 const app = express();
 const path = require("path");
 
+//define port
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()); //allow server to parse json requests
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/api", (req, res) => { //define the route
+app.get("/api", (req, res) => { //test the route
     res.json({ message: "Weather Task API is running" });
 });
 
-const axios = require("axios");
 
-app.get("/api/weather", async (req, res) => {
-    const city = req.query.city;
+app.get("/api/cities", async (req, res) => { //get city list
+  const city = req.query.city;
 
-    if (!city) {
-        return res.status(400).json({ error: "City is required" });
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.OPENWEATHER_API_KEY}`
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch cities" });
+  }
+});
+
+app.get("/api/weather", async (req, res) => {//get weather
+  const { city, lat, lon } = req.query;
+
+  let url;
+
+  if (lat && lon) { //search by lat and long
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.OPENWEATHER_API_KEY}`;
+  } 
+  else if (city) {//or search by name (not used in current frontend)
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.OPENWEATHER_API_KEY}`;
+  } 
+  else {
+    return res.json({ error: "City is required" });
+  }
+
+  try {
+    //gets the weather data 
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.cod !== 200) {
+      return res.json({ error: "City not found" });
     }
 
-    try {
-        const response = await axios.get(
-            "https://api.openweathermap.org/data/2.5/weather",
-            {
-                params: {
-                    q: `${city},US`,
-                    appid: process.env.OPENWEATHER_API_KEY,
-                    units: "imperial"
-                }
-            }
-        );
+    res.json({
+      city: data.name,
+      country: data.sys.country,
+      sunrise: data.sys.sunrise,
+      sunset: data.sys.sunset,
+      weather: {
+        temperature: data.main.temp,
+        temp_min: data.main.temp_min,
+        temp_max: data.main.temp_max,
+        humidity: data.main.humidity,
+        condition: data.weather[0].description
+      }
+    });
 
-        const data = response.data;
-
-        res.json({
-            city: data.name,
-            weather : {
-                temperature: Math.round(data.main.temp * 10) / 10, 
-                temp_min: Math.round(data.main.temp_min * 10) / 10,
-                temp_max: Math.round(data.main.temp_max * 10) / 10,
-                humidity: data.main.humidity,
-                condition: data.weather[0].description,
-            },
-            sunrise: data.sys.sunrise,
-            sunset: data.sys.sunset
-        });
-
-    } catch (error) {
-        if (error.response) {
-            return res.status(500).json({ error: "City not found" });
-        }
-
-        res.status(500).json({ error: "Weather service unavailable" });
-    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch weather" });
+  }
 });
 
 app.listen(PORT, (error) => { // runs server
